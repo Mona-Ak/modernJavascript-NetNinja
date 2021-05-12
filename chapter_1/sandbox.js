d9f419f5b3215ab29b495ae8803545e7d794ce6c
Mona <monaakbarpour1@gmail.com>
2021-05-12 14:32:38 +0200

    math operations

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -36,15 +36,55 @@
 // console.log(index);
 
 // __________________________________________
-// common string methods
+// // common string methods
 
-let email = 'monaakbarpour1@gmail.com';
-// let result = email.lastIndexOf('n');
+// let email = 'monaakbarpour1@gmail.com';
+// // let result = email.lastIndexOf('n');
 
-// let result = email.slice(2,6);
+// // let result = email.slice(2,6);
 
-// let result = email.substr(6,10);
+// // let result = email.substr(6,10);
 
-let result = email.replace('o' , 'i')
+// // let result = email.replace('o' , 'i');
 
-console.log(result);
+// console.log(result);
+
+// ______________________________________
+// let radius = 10;
+// const pi = 3.14;
+
+
+// // console.log(radius , pi);
+
+// // math operators +, -, *, /, **, %
+
+// // console.log(10 / 2);
+// // let result = radius % 3;
+// // let result = pi * radius**2;
+
+// // order of operation - B I D M A S
+
+// // let result = 5 * (10-3)**2;
+
+// // console.log(result);
+
+// let likes = 10;
+
+// // likes = likes + 1;
+// // likes++;
+// // likes--;
+
+// // likes += 10;
+// // likes -= 5;
+// // likes *= 2;
+// // likes /= 2;
+
+// // console.log(likes);
+
+// // NaN - nor a number
+// // console.log(5 / 'hello');
+
+// // let result = ' the blog has ' +
+// //  likes + ' likes';
+
+// //  console.log(result);
