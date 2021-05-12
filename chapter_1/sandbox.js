03a2325fd7033d08fd5f58da0ea110174622a2dd
Mona <monaakbarpour1@gmail.com>
2021-05-12 13:17:40 +0200

    learning string and working with it and knowing string methodes

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,7 +1,36 @@
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
-console.log(age , year);
+// console.log(age , year);
 
-const points = 100 ;
-console.log(points);
+// const points = 100 ;
+// console.log(points); 
+
+// ___________________________________________________
+// strings
+console.log('hello, world');
+
+let email = 'monaakbarpour1@gmail.com';
+console.log(email)
+
+// strings concatication
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName +' '+ lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string method
+
+console.log(fullName.toUpperCase());
+let result = fullName.toLocaleLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+console.log(index)
