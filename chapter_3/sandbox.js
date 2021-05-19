533109d6e08943547ce662942077defff90752e2
Mona <monaakbarpour1@gmail.com>
2021-05-19 19:43:31 +0200

    for loop and using it

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,1 +1,16 @@
 /* for loop */
+/* we don't put the semicolone at the end of the loop */
+
+/* for(let i = 0 ; i < 5; i++){
+    console.log('in loop :' , i);
+}
+
+console.log('loop finished'); */
+
+const names = ['shaun' , 'mario' , 'luigi'];
+
+for(let i = 0 ; i < names.length ; i ++){
+    // console.log(names[i]);
+    let html = `<div>${names[i]}</div>`;
+    console.log(html)
+}
