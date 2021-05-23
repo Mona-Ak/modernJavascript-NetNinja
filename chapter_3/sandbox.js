352c2a9563903b9dfe72c99fd41a5efd40a89910
Mona <monaakbarpour1@gmail.com>
2021-05-23 20:53:45 +0200

    learning while loop

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -6,11 +6,27 @@
 }
 
 console.log('loop finished'); */
-
+/* 
 const names = ['shaun' , 'mario' , 'luigi'];
 
 for(let i = 0 ; i < names.length ; i ++){
     // console.log(names[i]);
     let html = `<div>${names[i]}</div>`;
     console.log(html)
-}
+} */
+
+// while loops
+
+const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
+
+// while(i < 5){
+//     console.log('in loop :', i);
+//     i++
+// };
+
+let i = 0;
+while(i< names.length){
+    console.log(names   [i]);
+    i++;
+};
