487e1bcf03a481549192e8e71205670a322bcccf
Mona <monaakbarpour1@gmail.com>
2021-05-30 19:34:06 +0200

    arugemnts and parameters

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,17 +1,26 @@
-// **the diference between function declaration and function expression is that we can call the function declaration before it declares and this works because it is hoisting the function but in function expression is difference
+// // **the diference between function declaration and function expression is that we can call the function declaration before it declares and this works because it is hoisting the function but in function expression is difference
 
-//function expression
-// *anything that declares in variable we call it expression and at the needs semicolon
-const speak =function(){
-    console.log('good day!');
+// //function expression
+// // *anything that declares in variable we call it expression and at the needs semicolon
+// const speak =function(){
+//     console.log('good day!');
+// };
+
+// greet();
+
+// speak();
+
+// // function declaration
+// // *this function is not an expression that is why it does'nt need semicolon
+// function greet(){
+//     console.log('hello there');
+// }
+// ________________________________________________
+// arguments & parameters
+
+const speak = function(name = 'luigi', time = 'night'){
+    console.log(`good ${time} ${name}`);
 };
 
-greet();
-
 speak();
-
-// function declaration
-// *this function is not an expression that is why it does'nt need semicolon
-function greet(){
-    console.log('hello there');
-}
+speak('mona' , 'morning');
