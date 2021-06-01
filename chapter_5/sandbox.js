851bbff502bcc60030c59b0ccf9ffed1768a4c27
Mona <monaakbarpour1@gmail.com>
2021-06-01 20:30:20 +0200

    how to creating method and how to call it

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -5,18 +5,33 @@ let user = {
     age:20,
     email: 'monaakbarpour1@gmail.com',
     location:'solothurn',
-    blogs: ['why mac and chees rules' , '10 things to make with marmite']
+    blogs: ['why mac and chees rules' , '10 things to make with marmite'],
+    login:function(){
+        console.log('the user loged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    logBlogs:function(){
+        
+    }
 };
 
-console.log(user);
-console.log(user.name);
+// console.log(user);
+// console.log(user.name);
 
-// user.age = 35;
-console.log(user.age);
+// // user.age = 35;
+// console.log(user.age);
 
-// the other way to acces to the name in object
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user.name);
+// // the other way to acces to the name in object
+// console.log(user['name']);
+// user['name'] = 'chun-li';
+// console.log(user.name);
 
-console.log(typeof user);
+// console.log(typeof user);
+
+user.login();
+user.logout();
+
+const name = 'mario';
+name.toUpperCase();
