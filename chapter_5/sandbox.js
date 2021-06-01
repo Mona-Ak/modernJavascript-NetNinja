f9dbf96674d520deb71e1a3ae0b622589be54286
Mona <monaakbarpour1@gmail.com>
2021-06-01 21:30:25 +0200

    learning Math object

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,55 +1,77 @@
 // object literals
 
-// const blogs = [
-//     {title:'why mac and chees rules' , likes:30},
-//     {title: '10 things to make with marmite' , likes:50}
-// ];
+// // const blogs = [
+// //     {title:'why mac and chees rules' , likes:30},
+// //     {title: '10 things to make with marmite' , likes:50}
+// // ];
 
-// console.log(blogs);
+// // console.log(blogs);
 
-let user = {
-    name:'mona',
-    age:20,
-    email: 'monaakbarpour1@gmail.com',
-    location:'solothurn',
-    blogs: [
-        {title:'why mac and chees rules' , likes:30},
-        {title: '10 things to make with marmite' , likes:50}
-],
-    login:function(){
-        console.log('the user loged in');
-    },
-    logout: function(){
-        console.log('the user logged out');
-    },
-    logBlogs:function(){
-        // console.log(this.blogs);
-        console.log('this user has written the following blogs :');
-        this.blogs.forEach(blog => {
-            console.log(blog.title , blog.likes);
-        });
-    }
-};
+// let user = {
+//     name:'mona',
+//     age:20,
+//     email: 'monaakbarpour1@gmail.com',
+//     location:'solothurn',
+//     blogs: [
+//         {title:'why mac and chees rules' , likes:30},
+//         {title: '10 things to make with marmite' , likes:50}
+// ],
+//     login:function(){
+//         console.log('the user loged in');
+//     },
+//     logout: function(){
+//         console.log('the user logged out');
+//     },
+//     logBlogs:function(){
+//         // console.log(this.blogs);
+//         console.log('this user has written the following blogs :');
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title , blog.likes);
+//         });
+//     }
+// };
 
-// // console.log(user);
-// // console.log(user.name);
+// // // console.log(user);
+// // // console.log(user.name);
 
-// // // user.age = 35;
-// // console.log(user.age);
+// // // // user.age = 35;
+// // // console.log(user.age);
 
-// // // the other way to acces to the name in object
-// // console.log(user['name']);
-// // user['name'] = 'chun-li';
-// // console.log(user.name);
+// // // // the other way to acces to the name in object
+// // // console.log(user['name']);
+// // // user['name'] = 'chun-li';
+// // // console.log(user.name);
 
-// // console.log(typeof user);
+// // // console.log(typeof user);
 
-// user.login();
-// user.logout();
+// // user.login();
+// // user.logout();
 
-// const name = 'mario';
-// name.toUpperCase();
+// // const name = 'mario';
+// // name.toUpperCase();
 
-user.logBlogs();
+// user.logBlogs();
 
-// console.log(this);
+// // console.log(this);
+
+// ________________________________________________
+// Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 5.3;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// random nembers
+
+const random = Math.random();
+
+console.log(random);
+// the best way to take random number between 1-100
+console.log(Math.round(random * 100));
