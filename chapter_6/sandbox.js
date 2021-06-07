45507b6badba290478588d7f37cf55f9f7592a8d
Mona <monaakbarpour1@gmail.com>
2021-06-07 19:40:14 +0200

    innerHTML and innerText

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -25,5 +25,28 @@
 // console.log(paras[1]);
 
 // ______________________________________________
+const para = document.querySelector('p');
 
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome!';
 
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log( para.innerText);
+//     para.innerText += 'new text';
+// });
+
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>this is a new h2</h2>'
+
+const people = ['mario' , 'luigi' , 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`
+});
+
+// innerText ===> changes the content of a tag
+// ineerHTML ===> changes the content an element
