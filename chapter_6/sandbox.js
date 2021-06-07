e17ef22db88bda38e93fd80f641d280178fa95e7
Mona <monaakbarpour1@gmail.com>
2021-06-07 19:22:37 +0200

    using different ways to access to tags

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,9 +1,26 @@
-const para = document.querySelector('body > h1');
+// const para = document.querySelector('body > h1');
 
-console.log(para);
+// console.log(para);
 
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 
-const errors = document.querySelectorAll('.error');
+// const errors = document.querySelectorAll('.error');
 
+// console.log(errors);
+
+// ________________________________________________
+
+// get an element by ID
+const title = document.getElementById('page-title');
+console.log(title);
+
+// get elements by their class name 
+const errors = document.getElementsByClassName('error');
 console.log(errors);
+console.log(errors[0]);
+// **we can not use forEach function in HTMLcollection but we can use it in nodeList
+
+// get elements by thei tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
