f901844af6ee3e51377e58325d4973a3c5d3bb45
Mona <monaakbarpour1@gmail.com>
2021-06-07 19:24:56 +0200

    a little change in html file

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -9,18 +9,21 @@
 // console.log(errors);
 
 // ________________________________________________
+// // get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
 
-// get an element by ID
-const title = document.getElementById('page-title');
-console.log(title);
+// // get elements by their class name 
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+// // **we can not use forEach function in HTMLcollection but we can use it in nodeList
 
-// get elements by their class name 
-const errors = document.getElementsByClassName('error');
-console.log(errors);
-console.log(errors[0]);
-// **we can not use forEach function in HTMLcollection but we can use it in nodeList
+// // get elements by thei tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
 
-// get elements by thei tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[1]);
+// ______________________________________________
+
+
