d6960beca80435088e53f970d525afa6021f09b2
Mona <monaakbarpour1@gmail.com>
2021-06-15 21:00:10 +0200

    more DOM events: copy - mousemove - wheel

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -153,36 +153,56 @@
 // });
 
 // ______________________________________________________
-// creating and removing elements
-const ul = document.querySelector('ul');
-// ul.remove();
+// // creating and removing elements
+// const ul = document.querySelector('ul');
+// // ul.remove();
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-button.addEventListener('click' , e =>{
-    const li = document.createElement('li');
-    li.textContent = 'something new to do';
+// button.addEventListener('click' , e =>{
+//     const li = document.createElement('li');
+//     li.textContent = 'something new to do';
 
-    // append adds content after prepend adds conten before
-    // ul.append(li);
-    ul.prepend(li);
-});
-
-// const items = document.querySelectorAll('li');
-
-// items.forEach(item =>{
-//     item.addEventListener('click' , e =>{
-//         // e.target.style.textDecoration = 'line-through';
-//         console.log('event in LI');  
-//         e.stopPropagation();
-//         e.target.remove();
-//     })
+//     // append adds content after prepend adds conten before
+//     // ul.append(li);
+//     ul.prepend(li);
 // });
 
-ul.addEventListener('click', e => {
-    // console.log('event in UL');
-    // console.log(e.target);
-    if(e.target.tagName === 'LI'){
-        e.target.remove();
-    }
+// // const items = document.querySelectorAll('li');
+
+// //don't use the item down
+// // items.forEach(item =>{
+// //     item.addEventListener('click' , e =>{
+// //         // e.target.style.textDecoration = 'line-through';
+// //         console.log('event in LI');  
+// //         e.stopPropagation();
+// //         e.target.remove();
+// //     })
+// // });
+
+// ul.addEventListener('click', e => {
+//     // console.log('event in UL');
+//     // console.log(e.target);
+//     if(e.target.tagName === 'LI'){
+//         e.target.remove();
+//     }
+// });
+
+// ______________________________________________________
+// More Dom events
+
+const copy = document.querySelector('.copy-me');
+
+copy.addEventListener('copy' , ()=>{
+    console.log('OI!my content is copyright  ')
 });
+
+const box = document.querySelector('.box');
+box.addEventListener('mousemove' , e =>{
+    box.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY}`;
+});
+
+document.addEventListener('wheel' , e => {
+    console.log(`pageX - ${e.pageX} pageY - ${e.pageY}`);
+});
+
