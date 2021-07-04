8db548e27db0a49f943b8e432a387f6c403beeb9
Mona <monaakbarpour1@gmail.com>
2021-07-04 20:23:20 +0200

    learning reduce method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -19,28 +19,69 @@
 // console.log(premiumUsers);
 
 // _____________________________________________________________
-// map method
-const prices = [20, 10, 30, 25,15,40,80,5];
+// // map method
+// const prices = [20, 10, 30, 25,15,40,80,5];
 
-// const salePrices = prices.map(price =>price / 2);
-// console.log(salePrices);
+// // const salePrices = prices.map(price =>price / 2);
+// // console.log(salePrices);
 
-const products =[
-    {name:'gold star' , price:20 },
-    {name:'mushroom' , price: 40},
-    {name:'green shells' , price:30 },
-    {name:'banana skin' , price:10 },
-    {name:'red shells' , price: 50}
-];
+// const products =[
+//     {name:'gold star' , price:20 },
+//     {name:'mushroom' , price: 40},
+//     {name:'green shells' , price:30 },
+//     {name:'banana skin' , price:10 },
+//     {name:'red shells' , price: 50}
+// ];
 
-console.log(products);
+// console.log(products);
 
-const saleProducts = products.map(product => {
-    if(product.price > 30){
-        return {name: product.name , price : product.price / 2};
-    }else{
-        return product;
-    };
-});
+// const saleProducts = products.map(product => {
+//     if(product.price > 30){
+//         return {name: product.name , price : product.price / 2};
+//     }else{
+//         return product;
+//     };
+// });
 
-console.log(saleProducts);
+// console.log(saleProducts);
+
+// _____________________________________________________________
+// reduce method
+// const scores = [10,20,60,40,70,90,30];
+
+// const result = scores.reduce((acc , curr)=>{
+//     if(curr > 50){
+//         acc++;
+//     };
+//     return acc; 
+// }, 0);
+
+// console.log(result);
+
+ const scores = [
+     {player:'mario' , score :50},
+     {player:'yoshi' , score :30},
+     {player:'mario' , score :70},
+     {player:'crystal' , score :60},
+     {player:'mario' , score :50},
+     {player:'yoshi' , score :30},
+     {player:'mario' , score :70},
+     {player:'crystal' , score :60},
+     {player:'mario' , score :50},
+     {player:'yoshi' , score :30},
+     {player:'mario' , score :70},
+     {player:'crystal' , score :60},
+     {player:'mario' , score :50},
+     {player:'yoshi' , score :30},
+     {player:'mario' , score :70},
+     {player:'crystal' , score :60}
+ ];
+
+ const matioTotal = scores.reduce((acc , curr) =>{
+    if(curr.player === 'mario'){
+        acc += curr.score;
+    }
+    return acc;
+ },0);
+
+ console.log(matioTotal);
