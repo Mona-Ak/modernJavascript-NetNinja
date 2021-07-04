c6a4b15ea1888aefa3290784ea78f72d3a432855
Mona <monaakbarpour1@gmail.com>
2021-07-04 20:04:09 +0200

    using map method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,19 +1,46 @@
-// Filter method
-const scores = [10, 30, 15, 25, 50, 40, 5];
+// // Filter method
+// const scores = [10, 30, 15, 25, 50, 40, 5];
 
-// const filteredScores = scores.filter((score) =>{
-//     return score > 20;
-// });
+// // const filteredScores = scores.filter((score) =>{
+// //     return score > 20;
+// // });
 
-// console.log(filteredScores);
+// // console.log(filteredScores);
 
-const users =[
-    {name:'shaun' , premium:true},
-    {name:'yoshi' , premium:false},
-    {name:'mario' , premium:false},
-    {name:'chun-li' , premium:true},
+// const users =[
+//     {name:'shaun' , premium:true},
+//     {name:'yoshi' , premium:false},
+//     {name:'mario' , premium:false},
+//     {name:'chun-li' , premium:true}
+// ];
+
+// const premiumUsers = users.filter(user=> user.premium);
+
+// console.log(premiumUsers);
+
+// _____________________________________________________________
+// map method
+const prices = [20, 10, 30, 25,15,40,80,5];
+
+// const salePrices = prices.map(price =>price / 2);
+// console.log(salePrices);
+
+const products =[
+    {name:'gold star' , price:20 },
+    {name:'mushroom' , price: 40},
+    {name:'green shells' , price:30 },
+    {name:'banana skin' , price:10 },
+    {name:'red shells' , price: 50}
 ];
 
-const premiumUsers = users.filter(user=> user.premium);
+console.log(products);
 
-console.log(premiumUsers);
+const saleProducts = products.map(product => {
+    if(product.price > 30){
+        return {name: product.name , price : product.price / 2};
+    }else{
+        return product;
+    };
+});
+
+console.log(saleProducts);
