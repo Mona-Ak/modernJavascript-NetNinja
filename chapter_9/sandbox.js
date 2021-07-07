3b74767419ab88823d9dc5d20d32daca2b1fb875
Mona <monaakbarpour1@gmail.com>
2021-07-07 23:21:18 +0200

    setting up todo project

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -138,26 +138,26 @@
 // console.log(players);
 
 // __________________________________________________________
-// chaining array
-const products = [
-    {name: 'gold star' , price:30},
-    {name: 'green shell' , price:10},
-    {name: 'red shell' , price:40},
-    {name: 'banana skin' , price:5},
-    {name: 'mushroom' , price:50}
-];
+// // chaining array
+// const products = [
+//     {name: 'gold star' , price:30},
+//     {name: 'green shell' , price:10},
+//     {name: 'red shell' , price:40},
+//     {name: 'banana skin' , price:5},
+//     {name: 'mushroom' , price:50}
+// ];
 
-// const filtered = products.filter(product => product.price > 20);
+// // const filtered = products.filter(product => product.price > 20);
 
-// const promos = filtered.map(product => {
-//     return ` the ${product.name} is ${product.price / 2} pounds`;
-// });
+// // const promos = filtered.map(product => {
+// //     return ` the ${product.name} is ${product.price / 2} pounds`;
+// // });
 
-const promos = products
-.filter(product => product.price > 20)
-.map(product => {
-    return ` the ${product.name} is ${product.price / 2} pounds`; 
-})
+// const promos = products
+// .filter(product => product.price > 20)
+// .map(product => {
+//     return ` the ${product.name} is ${product.price / 2} pounds`; 
+// })
 
-console.log(promos);
-
+// console.log(promos);
+// _________________________________________________________________________
