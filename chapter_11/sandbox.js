c7c12f56459da78cda37df5c7b7d4de4760d6d57
Mona <monaakbarpour1@gmail.com>
2021-07-24 17:41:03 +0200

    making a digital clock

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -27,23 +27,46 @@
 // ____________________________________________________________________--
 // timestamps
 
-const before = new Date('February 1 2019 7:30:59');
-const now = new Date();
+// const before = new Date('February 1 2019 7:30:59');
+// const now = new Date();
 
-// console.log(now.getTime(),before.getTime());
+// // console.log(now.getTime(),before.getTime());
 
-const diff = now.getTime() - before.getTime();
-console.log(diff);
+// const diff = now.getTime() - before.getTime();
+// console.log(diff);
 
-// 1 second = 1000 milisecond  1 min = 60 second
-const mins = Math.round(diff / 1000/ 60); //minute
-const hours = Math.round(mins / 60); //hour
-const days = Math.round(hours / 24); //hour
-console.log(mins, hours , days);
+// // 1 second = 1000 milisecond  1 min = 60 second
+// const mins = Math.round(diff / 1000/ 60); //minute
+// const hours = Math.round(mins / 60); //hour
+// const days = Math.round(hours / 24); //hour
+// console.log(mins, hours , days);
 
-console.log(`the blog was written ${days} ago`);
+// console.log(`the blog was written ${days} ago`);
 
-// converting timestamps into date objects
-const timestamp = 1675938474990;
-console.log(new Date(timestamp));
+// // converting timestamps into date objects
+// const timestamp = 1675938474990;
+// console.log(new Date(timestamp));
+// ___________________________________________
+const clock = document.querySelector('.clock');
+
+const tick = () => {
+
+    const now = new Date();
+
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+
+    const html = `
+        <span>${h}</span> :
+        <span>${m}</span> :
+        <span>${s}</span>
+    `;
+
+    clock.innerHTML = html;
+
+};
+
+// calling function every second
+setInterval(tick, 1000);
 
