f25ef1477d83b91fe03228fe5215bfe0b49b918f
Mona <monaakbarpour1@gmail.com>
2021-07-26 13:42:48 +0200

    learnin async code in action

---
+++ chapter_12/sandbox.js
@@ -0,0 +1,10 @@
+// learning async code in action how works 
+console.log(1);
+console.log(2);
+
+setTimeout(() =>{
+    console.log('callback function fired');
+} , 2000);
+
+console.log(3);
+console.log(4);
