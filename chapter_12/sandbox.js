68968fd27918a99b77feb0f0ec78019f1b25fa1c
Mona <monaakbarpour1@gmail.com>
2021-07-26 18:05:49 +0200

    making http request

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,10 +1,26 @@
-// learning async code in action how works 
-console.log(1);
-console.log(2);
+// // learning async code in action how works 
+// console.log(1);
+// console.log(2);
 
-setTimeout(() =>{
-    console.log('callback function fired');
-} , 2000);
+// setTimeout(() =>{
+//     console.log('callback function fired');
+// } , 2000);
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
+
+// ________________________________________
+// making http request
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', ()=>{
+    // console.log(request, request.readyState);
+    if(request.readyState === 4){
+        console.log(request.responseText);
+    }
+});
+
+request.open('GET' ,'https://jsonplaceholder.typicode.com/todos/');
+
+request.send();
