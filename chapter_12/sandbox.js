a488ecc70d0a046cacbac8767b95ed196f652240
Mona <monaakbarpour1@gmail.com>
2021-07-31 15:30:51 +0200

    learning promise basic

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -12,37 +12,61 @@
 // ________________________________________
 // making http request
 
-const getTodos = (resource,callback) => {
-  const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    // console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-      // converting json file to object
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback('could not fetch data', undefined);
-    }
+    request.addEventListener("readystatechange", () => {
+      // console.log(request, request.readyState);
+      if (request.readyState === 4 && request.status === 200) {
+        // converting json file to object
+        const data = JSON.parse(request.responseText);
+        
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting source');
+      }
+    });
+
+    request.open("GET", resource);
+
+    request.send();
   });
-
-  request.open("GET", resource);
-
-  request.send();
 };
 
-
-getTodos('todos/luigi.json',(err,data)=>{
-  console.log(data);
-
-  getTodos('todos/mario.json' , (err,data) =>{
-    console.log(data);
-
-    getTodos('todos/shaun.json' , (err,data)=>{
-      console.log(data);
-    })
-
-  })
-
+getTodos('todos/luigi.json').then(data => {
+  console.log('promise resolved:' , data);
+}).catch(err =>{
+  console.log('promise rejected:' ,err);
 });
 
+// promise example
+
+// const getSomething = () =>{
+
+//   return new Promise((resolve,reject)=>{
+//     // fetch something
+//     resolve('some data');
+//     // reject('some error');
+//   });
+
+// };
+
+// the first function in then method is for resoleve and the scond one is for reject
+
+// getSomething().then((data)=>{
+
+//   console.log('this is ' , data);
+// }, (err)=>{
+//   console.log(err);
+// });
+
+// another way of writting function above
+
+getSomething()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
