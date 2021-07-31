6dfb31eafad767a14e22588a271b9c29ea8819c3
Mona <monaakbarpour1@gmail.com>
2021-07-31 17:05:27 +0200

    the way that we can fetching an API

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -10,43 +10,43 @@
 // console.log(4);
 
 // ________________________________________
-// making http request
+// // making http request
 
-const getTodos = (resource) => {
-  // we are using promise because Promises are used to handle asynchronous operations in JavaScript.
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+// const getTodos = (resource) => {
+//   // we are using promise because Promises are used to handle asynchronous operations in JavaScript.
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
 
-    request.addEventListener("readystatechange", () => {
-      // console.log(request, request.readyState);
-      if (request.readyState === 4 && request.status === 200) {
-        // converting json file to object
-        const data = JSON.parse(request.responseText);
+//     request.addEventListener("readystatechange", () => {
+//       // console.log(request, request.readyState);
+//       if (request.readyState === 4 && request.status === 200) {
+//         // converting json file to object
+//         const data = JSON.parse(request.responseText);
         
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject('error getting source');
-      }
-    });
+//         resolve(data);
+//       } else if (request.readyState === 4) {
+//         reject('error getting source');
+//       }
+//     });
 
-    request.open("GET", resource);
+//     request.open("GET", resource);
 
-    request.send();
-  });
-};
+//     request.send();
+//   });
+// };
 
-// chaining promises
-getTodos('todos/luigi.json').then(data => {
-  console.log('promise 1 resolved:' , data);
-  return getTodos('todos/mario.json');
-}).then(data =>{
-  console.log('promise 2 resolved:' , data);
-  return getTodos('todos/shaun.json');
-}).then(data =>{
-  console.log('promise 3 resolved: ',data);
-}).catch(err =>{
-  console.log('promise rejected:' ,err);
-});
+// // chaining promises
+// getTodos('todos/luigi.json').then(data => {
+//   console.log('promise 1 resolved:' , data);
+//   return getTodos('todos/mario.json');
+// }).then(data =>{
+//   console.log('promise 2 resolved:' , data);
+//   return getTodos('todos/shaun.json');
+// }).then(data =>{
+//   console.log('promise 3 resolved: ',data);
+// }).catch(err =>{
+//   console.log('promise rejected:' ,err);
+// });
 
 // promise example
 
@@ -78,3 +78,18 @@ getTodos('todos/luigi.json').then(data => {
 //   .catch((err) => {
 //     console.log(err);
 //   });
+
+// _________________________________________________________
+// fetch api
+
+// it is also promise 
+fetch('todos/luigi.json').then(response => {
+  console.log('resolved' , response);
+  // return response.json(); returns the promise
+  return response.json();
+}).then(data =>{
+  // here we can get the real data after returning the response
+  console.log(data);
+}).catch(err =>{
+  console.log('rejected' , err);
+});
