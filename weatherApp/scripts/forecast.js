14c8534e9fa005c63d1331a0672f7e904ee6eb83
Mona <monaakbarpour1@gmail.com>
2021-08-30 00:39:59 +0200

    get weather api call

--- weatherApp/scripts/forecast.js
+++ weatherApp/scripts/forecast.js
@@ -1,5 +1,20 @@
 const key = 'WGg851pKdwxKAOW9QD7VdyXQ1gKlGLy8';
 
+// get weather information
+const getWeather = async(id)=>{
+
+    const base= 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response =await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+
+// get city information
 const getCity = async (city)=>{
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
@@ -12,6 +27,9 @@ const getCity = async (city)=>{
 };
 
 getCity('manchester')
-    .then(data=> console.log(data))
-    .catch(err => console.log(err));
+    .then(data=>{
+        return getWeather(data.Key);
+    }).then(data =>{
+        console.log(data);
+    }).catch(err => console.log(err));
 
